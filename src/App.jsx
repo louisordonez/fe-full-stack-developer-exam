7504b914bbd9965f7e693d0e316ff122e6fbe748
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-12-09 14:39:41 +0800

    Create launches component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Launches from './components/Launches/Launches'
+import './App.css'
+
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Launches />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Launches/Launches.jsx
@@ -0,0 +1,13 @@
+import SearchInput from './Input/SearchInput'
+import './Launches.css'
+
+const Launches = () => {
+  return (
+    <div className="launches-container">
+      Launches
+      <SearchInput />
+    </div>
+  )
+}
+
+export default Launches
